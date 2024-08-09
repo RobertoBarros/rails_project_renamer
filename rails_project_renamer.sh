8d2f5417449da98fa47f60173ca3c3c24530d9d0
Roberto Barros <roberto.f.barros@gmail.com>
2024-08-09 20:33:30 -0300

    rename

--- rails_project_renamer.sh
+++ rails_project_renamer.sh
@@ -29,15 +29,24 @@ git init
 # Extract the old name from the Rails module in the config/application.rb file
 OLD_NAME=$(grep "module " config/application.rb | sed -e 's/module \(.*\)/\1/' -e 's/ *$//')
 
-# Convert names to different formats
-OLD_NAME_SNAKE="$(echo $OLD_NAME | sed -r 's/([A-Z])/_\L\1/g' | cut -c 2-)"
-NEW_NAME_SNAKE="$(echo $NEW_NAME | sed -r 's/([A-Z])/_\L\1/g' | cut -c 2-)"
+# Convert OLD_NAME to snake_case
+OLD_NAME_SNAKE=$(echo "$OLD_NAME" | sed -r 's/([a-z0-9])([A-Z])/\1_\2/g' | tr '[:upper:]' '[:lower:]')
+
+# Convert NEW_NAME to snake_case
+NEW_NAME_SNAKE=$(echo "$NEW_NAME" | sed -r 's/([a-z0-9])([A-Z])/\1_\2/g' | tr '[:upper:]' '[:lower:]')
+
+# Convert OLD_NAME to uppercase with underscores for environment variables
+OLD_NAME_ENV=$(echo "$OLD_NAME_SNAKE" | tr '[:lower:]' '[:upper:]')
+
+# Convert NEW_NAME to uppercase with underscores for environment variables
+NEW_NAME_ENV=$(echo "$NEW_NAME_SNAKE" | tr '[:lower:]' '[:upper:]')
 
 # Replace all occurrences of the old name with the new name across all project files
 find . -type f \( -name "*.rb" -o -name "*.erb" -o -name "*.yml" -o -name "*.js" -o -name "*.css" \) -exec sed -i "" \
 -e "s/${OLD_NAME}/${NEW_NAME}/g" \
 -e "s/${OLD_NAME_SNAKE}/${NEW_NAME_SNAKE}/g" \
--e "s/${OLD_NAME_SNAKE}_/${NEW_NAME_SNAKE}_/g" {} +
+-e "s/${OLD_NAME_SNAKE}_/${NEW_NAME_SNAKE}_/g" \
+-e "s/${OLD_NAME_ENV}/${NEW_NAME_ENV}/g" {} +
 
 # Add all files to the new repository and commit them
 git add .
